c1659c615247c4262280eb391bb86834437c4767
Mark Dawkins <mark.dawkins94@gmail.com>
2021-04-05 22:32:36 +1000

    first draft of movement rules

--- Game.py
+++ Game.py
@@ -1,8 +1,6 @@
 from Checkersmate.Display import print_turn, print_piece, print_board
 
 class Game():
-
-
     def __init__(self,
     board=[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-1,-1],
     turn=1,
@@ -16,3 +14,6 @@ class Game():
 	    return f''' ___________Checkersmate____________
     {print_turn(self.turn)}'s turn 
 {print_board(self.board,self.turn)}'''	
+
+    def copy_game(self):
+        return Game(self.board,self.turn,self.msc)
--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 from Checkersmate.Game import Game
-from Checkersmate.Rules import legal_moves
+# from Checkersmate.Rules import legal_moves
 from Checkersmate.Result import check_result
 
 if __name__ == '__main__':
-    players = {1:Player(),-1:Player()}
+    # players = {1:Player(),-1:Player()}
     result = 0
     current_state = Game()
     while result==0:
@@ -11,5 +11,6 @@ if __name__ == '__main__':
         # potential_states = legal_moves(current_state) 
         # new_state = players[current_state.turn].choose_move(potential_new_states)
         new_state = current_state
+        # state.msc update
         result = check_result(new_state)
-        
+        # current_state = new_state.change_turn()
